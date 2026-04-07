ec4f6891fd87b10aa0efd2e87a908a0c05088b00
Fauzan Pradana <fauzanpr.06@gmail.com>
2026-04-07 17:04:34 +0700

    feat(su/tables): adjust zip download

--- src/features/superuser/tables/components/qr/GenerateBulkQRDialog.tsx
+++ src/features/superuser/tables/components/qr/GenerateBulkQRDialog.tsx
@@ -52,7 +52,7 @@ function GenerateBulkQRDialog({ onClose, open }: TGenerateBulkQRDialog) {
     const { mutate, isPending } = useGenerateTableQRMutation({
         onSuccess: (res) => {
             toast.success("Success");
-            handleFileResponse(res.data, "image/png");
+            handleFileResponse(res.data, isAllTables ? "application/zip" : "image/png");
         },
         onError: (err) => {
             toast.error(getErrorMessage(err));
